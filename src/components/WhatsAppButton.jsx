e3d250d05d75b2f38a82ec1f3fb6575d595cad1d
Fábio <fabiorodrigues.programasao@gmail.com>
2026-04-20 19:44:31 -0300

    Corrigido build final

--- src/components/WhatsAppButton.jsx
+++ src/components/WhatsAppButton.jsx
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
 import { FaWhatsapp } from "react-icons/fa";
-import "../styles/style.css"
+import "../styles/style.css";
 
 export default function WhatsAppButton() {
     return (
@@ -14,21 +13,4 @@ export default function WhatsAppButton() {
             <FaWhatsapp />
         </a>
     );
-=======
-import { FaWhatsapp } from "react-icons/fa";
-import "../styles/style.css"
-
-export default function WhatsAppButton() {
-    return (
-        <a
-            href="https://wa.me/5561XXXXXXXXX?text=Olá,%20vim%20pelo%20site%20e%20quero%20informações%20sobre%20os%20produtos"
-            className="whatsapp-float"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="WhatsApp"
-        >
-            <FaWhatsapp />
-        </a>
-    );
->>>>>>> f9e0a262f9453b2e992fb3f67c91fb13f5966766
 }
