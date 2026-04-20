7fd0ed5e209a61290f9a1c1d99ee3415cc1ac7de
Fábio <fabiorodrigues.programasao@gmail.com>
2026-04-20 19:32:41 -0300

    Corrigido conflitos git

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { FaInstagram, FaFacebookF } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import "../styles/style.css";
@@ -72,58 +71,4 @@ export default function Footer() {
 
         </footer>
     );
-=======
-import { FaInstagram, FaFacebookF } from "react-icons/fa";
-import { MdEmail } from "react-icons/md";
-import "../styles/style.css";
-
-export default function Footer() {
-    return (
-        <footer id="contato" className="footer">
-
-            {/* TOPO */}
-            <div className="footer-top">
-
-                {/* ESQUERDA */}
-                <div className="footer-left">
-                    <h2 className="footer-logo">Enlace das Arteiras</h2>
-                </div>
-
-                {/* DIREITA */}
-                <div className="footer-right rede">
-                    <h3>SIGA NOSSAS REDES</h3>
-
-                    <div className="social-links">
-                        <a href="https://www.instagram.com/enlacedasarteiras/" target="_blank" aria-label="Instagram">
-                            <FaInstagram />
-                        </a>
-
-                        <a href="https://facebook.com" target="_blank" aria-label="Facebook">
-                            <FaFacebookF />
-                        </a>
-
-                        <a href="mailto:contato@email.com" aria-label="Email">
-                            <MdEmail />
-                        </a>
-                    </div>
-                </div>
-
-            </div>
-
-            {/* MENU */}
-            <div className="footer-menu">
-                <a href="/">Home</a>
-                <a href="#cursos">Cursos</a>
-                <a href="#feiras">Feiras</a>
-                <a href="#parceiros">Parceiros</a>
-            </div>
-
-            {/* COPY */}
-            <div className="footer-copy">
-                <p>© 2026 Enlace das Arteiras</p>
-            </div>
-
-        </footer>
-    );
->>>>>>> f9e0a262f9453b2e992fb3f67c91fb13f5966766
 }
