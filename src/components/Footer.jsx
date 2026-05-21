fc83e49b7a6f8c90426590e709151e2fd221881b
Fábio <fabiorodrigues.programasao@gmail.com>
2026-05-20 23:26:12 -0300

    Atualização do site

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,30 @@
 import { FaInstagram, FaFacebookF, FaWhatsapp } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 
-import footerLogos from "../assets/Footer Aprovado - Novo 1_20260520_224710_0000.png";
+/* LOGOS */
+
+import logoFac from "../assets/Logo - Fac.png";
+import logoEnlace from "../assets/logo-Nova.png";
+import logoMariba from "../assets/Logo - Mariba.png";
+
+import logoMovimento from "../assets/Logo - Movimento.png";
+import logoBrasiliaAmbiental from "../assets/Logo - Brasilia.png";
+import logoSecretaria from "../assets/Logo - Secretaria.png";
 
 import "../styles/style.css";
 
 export default function Footer() {
+
     return (
+
         <footer id="contato" className="footer">
 
+            {/* TOPO */}
             <div className="footer-top">
+
+                {/* ESQUERDA */}
                 <div className="footer-left">
+
                     <span className="footer-subtitle">
                         Cultura • Arte • Sustentabilidade
                     </span>
@@ -24,9 +38,12 @@ export default function Footer() {
                         cultura, empreendedorismo feminino e
                         sustentabilidade.
                     </p>
+
                 </div>
 
+                {/* DIREITA */}
                 <div className="footer-right">
+
                     <h3>Siga nossas redes</h3>
 
                     <p className="footer-social-text">
@@ -35,6 +52,7 @@ export default function Footer() {
                     </p>
 
                     <div className="social-links">
+
                         <a
                             href="https://www.instagram.com/enlacedasarteiras/"
                             target="_blank"
@@ -70,37 +88,116 @@ export default function Footer() {
                         >
                             <MdEmail />
                         </a>
+
                     </div>
+
                 </div>
+
             </div>
 
+            {/* MENU */}
             <nav className="footer-menu">
-                <a href="/">Início</a>
-                <a href="/secec-df">SECEC-DF</a>
-                <a href="/artesanato-manualismo">Artesanato</a>
-                <a href="/gastronomia-artesanal">Gastronomia</a>
-                <a href="/cursos">Cursos</a>
-                <a href="/feiras">Feiras</a>
-                <a href="/galeria">Galeria</a>
-                <a href="/parceiros">Parceiros</a>
+
+                <a href="/">
+                    Início
+                </a>
+
+                <a href="/secec-df">
+                    SECEC-DF
+                </a>
+
+                <a href="/artesanato-manualismo">
+                    Artesanato
+                </a>
+
+                <a href="/gastronomia-artesanal">
+                    Gastronomia
+                </a>
+
+                <a href="/cursos">
+                    Cursos
+                </a>
+
+                <a href="/feiras">
+                    Feiras
+                </a>
+
+                <a href="/galeria">
+                    Galeria
+                </a>
+
+                <a href="/parceiros">
+                    Parceiros
+                </a>
+
             </nav>
 
+            {/* LOGOS */}
             <div className="footer-logos">
 
-                <div className="footer-logos-image">
+                <p className="footer-logos-text">
+                    Este projeto é realizado com recursos do Fundo de Apoio à Cultura do Distrito Federal.
+                </p>
+
+                {/* LOGOS PRINCIPAIS */}
+                <div className="footer-logos-principais">
+
                     <img
-                        src={footerLogos}
-                        alt="Logos de apoiadores e parceiros do projeto"
+                        src={logoFac}
+                        alt="FAC"
                     />
+
+                    <img
+                        src={logoEnlace}
+                        alt="Enlace das Arteiras"
+                    />
+
+                    <img
+                        src={logoMariba}
+                        alt="Maribá Produções"
+                    />
+
                 </div>
+
+                {/* APOIO */}
+                <div className="footer-apoio">
+
+                    <h3>APOIO:</h3>
+
+                    <div className="footer-logos-apoio">
+
+                        <img
+                            src={logoMovimento}
+                            alt="Movimento Comunitário"
+                        />
+
+                        <img
+                            src={logoBrasiliaAmbiental}
+                            alt="Brasília Ambiental"
+                        />
+
+                        <img
+                            src={logoSecretaria}
+                            alt="Secretaria de Cultura"
+                        />
+
+                    </div>
+
+                </div>
+
             </div>
 
+            {/* COPY */}
             <div className="footer-copy">
+
                 <p>
-                    © 2026 Enlace das Arteiras • Todos os direitos reservados
+                    © 2026 Enlace das Arteiras •
+                    Todos os direitos reservados
                 </p>
+
             </div>
 
         </footer>
+
     );
 }
